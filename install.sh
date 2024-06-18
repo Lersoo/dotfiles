fe70232d97f6128d7700ffae669a901e83b67ebe
Thomas <soler.thomas.06@gmail.com>
2024-06-18 16:51:05 +0200

    Update install script

--- install.sh
+++ install.sh
@@ -1,3 +1,65 @@
+#!/bin/bash
+
+REGULAR="\\033[0;39m"
+YELLOW="\\033[1;33m"
+GREEN="\\033[1;32m"
+
+# zsh plugins
+CURRENT_DIR=`pwd`
+cd "$CURRENT_DIR"
+
+if [[ ! `uname` =~ "darwin" ]]; then
+  # Cleanup
+  read -p "Do you want to cleanup? (y/n) " -n 1 -r
+  echo
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    [ -f ~/.zshrc ] && rm ~/.zshrc
+    [ -f ~/.bashrc ] && rm ~/.bashrc
+    [ -f ~/.bash_profile ] && rm ~/.bash_profile
+    [ -f ~/.profile ] && rm ~/.profile
+    [ -f ~/.bash_logout ] && rm ~/.bash_logout
+  else
+    echo "Skipping cleanup"
+  fi
+
+  # Install Homebrew
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+  # Update package list
+  brew update
+  brew upgrade
+
+  # Install zsh
+  brew install zsh
+
+  # Install oh-my-zsh & spaceship theme
+  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
+  ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+
+  # Install zsh plugins
+  git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+
+  # Install packages
+  brew install bat
+  brew install diff-so-fancy
+  brew install eza
+  brew install fzf
+  brew install git
+  brew install heroku
+  brew install neovim
+  brew install warp
+  brew install zed
+
+  # Install NVM
+  sh -c $(curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh)
+
+  # Install rbenv
+  brew install rbenv ruby-build
+
+fi
+
 backup() {
   target=$1
   if [ -e "$target" ]; then           # Does the config file already exist?
@@ -8,11 +70,10 @@ backup() {
   fi
 }
 
-#!/bin/zsh
 for name in *; do
   if [ ! -d "$name" ]; then
     target="$HOME/.$name"
-    if [[ ! "$name" =~ '\.sh$' ]] && [ "$name" != 'README.md' ] && [[ ! "$name" =~ '\.sublime-settings$' ]]; then
+    if [[ ! "$name" =~ '\.sh$' ]] && [ "$name" != 'README.md' ]; then
       backup $target
 
       if [ ! -e "$target" ]; then
@@ -23,25 +84,4 @@ for name in *; do
   fi
 done
 
-REGULAR="\\033[0;39m"
-YELLOW="\\033[1;33m"
-GREEN="\\033[1;32m"
-
-# zsh plugins
-CURRENT_DIR=`pwd`
-ZSH_PLUGINS_DIR="$HOME/.oh-my-zsh/custom/plugins"
-cd "$CURRENT_DIR"
-
-setopt nocasematch
-if [[ ! `uname` =~ "darwin" ]]; then
-  git config --global core.editor "subl -n -w $@ >/dev/null 2>&1"
-  echo 'export BUNDLER_EDITOR="subl $@ >/dev/null 2>&1 -a"' >> zshrc
-else
-  git config --global core.editor "'/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl' -n -w"
-  bundler_editor="'/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl'"
-  echo "export BUNDLER_EDITOR=\"${bundler_editor} -a\"" >> zshrc
-fi
-
-zsh ~/.zshrc
-
-echo "👌  Carry on with git setup!"
+exec zsh
--- zshrc
+++ zshrc
@@ -1,19 +1,18 @@
-ZSH="$HOME/.oh-my-zsh"
+export ZSH="$HOME/.oh-my-zsh"
 ZSH_THEME="spaceship"
 
-# Oh-my-zsh plugins
-plugins=(git
-        gitfast
-        common-aliases
-        rails
-        rake-fast
-        pj
-        zsh-syntax-highlighting
-        history-substring-search)
+plugins=(common-aliases
+          git
+          gitfast
+          history-substring-search
+          pj
+          rails
+          zsh-autosuggestions
+          zsh-syntax-highlighting)
 
 PROJECT_PATHS=(~/code/) # for the pj plugin
-source "${ZSH}/oh-my-zsh.sh"
-unalias rm # No interactive rm by default (brought by plugins/common-aliases)
+source $ZSH/oh-my-zsh.sh
+unalias rm
 
 export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
@@ -24,13 +23,10 @@ export EDITOR="zed --wait"
 export PATH="${HOME}/.rbenv/bin:${PATH}"
 type -a rbenv > /dev/null && eval "$(rbenv init -)"
 
-# pyenv
-export PATH="${HOME}/.pyenv/bin:${PATH}"
-type -a pyenv > /dev/null && eval "$(pyenv init -)" && eval "$(pyenv virtualenv-init -)"
-
 # nvm
 export NVM_DIR="$HOME/.nvm"
-[ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 # Allow running binstubs without `bin/` prefix
 export PATH="./bin:./node_modules/.bin:${PATH}:/usr/local/sbin"
